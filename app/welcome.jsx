65a5ed12dcd35dd7cf08c4655849c4c12b7ced8b
Yulian <105125828+YulianGuinand@users.noreply.github.com>
2024-10-11 22:43:59 +0200

    Feat(WelcomePage): Creation de la welcome page

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -1,13 +1,100 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
 import ScreenWrapper from "../components/ScreenWrapper";
+import { theme } from "../constants/theme";
+import { hp, wp } from "../helpers/common";
 
 const welcome = () => {
   return (
-    <ScreenWrapper>
-      <Text>welcome</Text>
+    <ScreenWrapper bg="white">
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        {/* WELCOME IMAGE */}
+        <Image
+          style={styles.welcomeImage}
+          resizeMode="contain"
+          source={require("../assets/images/welcome.png")}
+        />
+        {/* TITLE */}
+        <View style={{ gap: 20 }}>
+          <Text style={styles.title}>LinkUp!</Text>
+          <Text style={styles.punchline}>
+            Where every thought finds a home and every images tells stories!
+          </Text>
+        </View>
+
+        {/* FOOTER */}
+        <View style={styles.footer}>
+          <Button
+            title="Getting Started"
+            buttonStyle={{ marginHorizontal: wp(3) }}
+            onPress={() => {}}
+          />
+
+          <View style={styles.bottomTextContainer}>
+            <Text style={styles.loginText}>Already have an account ?</Text>
+            <Pressable>
+              <Text
+                style={[
+                  styles.loginText,
+                  {
+                    color: theme.colors.primaryDark,
+                    fontWeight: theme.fonts.semibold,
+                  },
+                ]}
+              >
+                Login
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "white",
+    marginHorizontal: wp(4),
+  },
+  welcomeImage: {
+    height: hp(30),
+    width: wp(100),
+    alignSelf: "center",
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(4),
+    textAlign: "center",
+    fontWeight: theme.fonts.extraBold,
+  },
+  punchline: {
+    textAlign: "center",
+    paddingHorizontal: wp(10),
+    fontSize: hp(1.7),
+    color: theme.colors.text,
+  },
+  footer: {
+    gap: 30,
+    width: "100%",
+  },
+  bottomTextContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  loginText: {
+    textAlign: "center",
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  },
+});
