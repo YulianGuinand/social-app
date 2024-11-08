f3aa8afd07bf1e0965053f12808745838aec939a
Yulian <58071866+RectoVersoDev@users.noreply.github.com>
2024-11-08 12:27:05 +0100

    Feat(Group): Header ui

--- app/groups/[id].jsx
+++ app/groups/[id].jsx
@@ -1,15 +1,20 @@
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Header from "../../components/shared/Header";
+import ScreenWrapper from "../../components/shared/ScreenWrapper";
+import { wp } from "../../helpers/common";
 
 const GroupScreen = () => {
   const { id, data } = useLocalSearchParams();
+  const group = JSON.parse(data).group_id;
 
-  console.log(id, data);
   return (
-    <View>
-      <Text>GroupScreen</Text>
-    </View>
+    <ScreenWrapper>
+      <View style={{ paddingHorizontal: wp(4) }}>
+        <Header title={group.name} />
+      </View>
+    </ScreenWrapper>
   );
 };
 
